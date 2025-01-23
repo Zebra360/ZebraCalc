2edad510f515fec44c99811e7086afb706b9e3b2
Zebra360 <md.reja0650@gmail.com>
2025-01-23 13:25:48 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,39 +1,44 @@
-let str = '';
+function calculator() {
+  let calcHistory = document.querySelector('.calc-history');
 
-let buttons = document.querySelectorAll('.btn');
-let inputVal = document.getElementById('calc-display');
-let calcHistoryIcon = document.getElementById('history-icon');
-let calcHistory = document.querySelector('.calc-history');
 
-Array.from(buttons).forEach((button) => {
-  button.addEventListener('click', (e) => {
-    if (e.target.innerHTML == '=') {
-      str = eval(str);
-      inputVal.value = str;
+  document.addEventListener('click', (e) => {
+    let input = document.getElementById('calc-display');
 
-      let aNewHis = document.createElement('p');
-      aNewHis.innerHTML = `&nbsp;${str}&nbsp;||&nbsp;`;
-      calcHistory.append(aNewHis);
-    } else if (e.target.innerHTML == "AC") {
-      str = '';
-      inputVal.value = str;
-    } else if (e.target.innerHTML == "X") {
-      str = str.slice(0, -1);
-      inputVal.value = str;
-    } else if (e.target.innerHTML == '+/-') {
-      str = `(-${str})`
-      inputVal.value = str;
+    if (!e.target.classList.contains('btn')) return;
+    else if (e.target.innerHTML === '=') {
+      input.value = eval(input.value);
+      let newHistory = document.createElement('p');
+      newHistory.innerHTML = `&nbsp;${input.value}&nbsp;||&nbsp;`;
+      calcHistory.append(newHistory);
+    } else if (e.target.innerHTML === 'C') {
+      input.value = input.value.slice(0, -1);
+    } else if (e.target.innerHTML === '+/-') {
+      input.value = `(-${input.value})`
+    } else if (e.target.innerHTML === 'AC') {
+      input.value = '';
     } else {
-      str += e.target.innerHTML;
-      inputVal.value = str;
-    }
+      input.value += e.target.innerHTML;
+    };
   });
-});
 
-calcHistoryIcon.addEventListener('click', () => {
-  if (calcHistory.style.bottom === '-100vh') {
-    calcHistory.style.bottom = '0';
-  } else {
-    calcHistory.style.bottom = '-100vh';
-  }
-});
+
+
+  function history() {
+    let calcHistoryIcon = document.getElementById('history-icon');
+
+    calcHistory.style.bottom = calcHistory.style.bottom || '-100vh';
+
+    calcHistoryIcon.addEventListener('click', () => {
+      if (calcHistory.style.bottom === '-100vh') {
+        calcHistory.style.bottom = '0';
+      } else {
+        calcHistory.style.bottom = '-100vh';
+      }
+    });
+  };
+  history();
+};
+
+
+calculator();
